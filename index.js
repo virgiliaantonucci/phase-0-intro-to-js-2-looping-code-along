8455d4211d471a93cde3aaae246fef4c5e4b20af
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-04-14 18:15:02 -0400

    commit tree 0756ad9a9a3cc900bfb69255be87305728c1ebba

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  } */
+
+  function writeCards(namesArray, eventString){
+    let newArray = []
+
+    for (let i = 0; i < namesArray.length; i++) {
+      newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventString} gift!`);
+    }  
+      
+    return newArray;
+  }
+
+  function countDown(countdown){
+
+    while (countdown > -1) {
+      console.log(countdown--); 
+    }
+  }
